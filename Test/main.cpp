2b918d1a9b804efd78163e3a48630df2582a7b6f
Seth Roberts <sar242004@gmail.com>
2025-04-26 22:11:56 -0500

    ef

--- Test/main.cpp
+++ Test/main.cpp
@@ -296,7 +296,7 @@ int client_main() {
 			char great[7] = "GREAT!";
 
 			recvfrom(ConnectionlessSocket, recvBuf, DEFAULT_BUFLEN, 0, (sockaddr*)&addr, &addrSize);
-			wait(ConnectionlessSocket, 15, 0);
+			wait(ConnectionlessSocket, 7, 0);
 
 			if (_stricmp(recvBuf, "YES") == 0) {//if server said yes
 				int iResult = sendto(ConnectionlessSocket, great, strlen(great) + 1, 0, (sockaddr*)&serverInfo[i].addr, sizeof(serverInfo[i].addr));
